5961d0cc0a3c86872d32094e79e4a553b1726534
HashirMusthafa <hashirvaliyil8146@gmail.com>
2025-09-30 13:50:45 +0530

    Feat: Added Basic configuration of next app

---
+++ Client/next.config.ts
@@ -0,0 +1,36 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "static.vecteezy.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", // Add CSP for security
+  },
+};
+
+export default nextConfig;
