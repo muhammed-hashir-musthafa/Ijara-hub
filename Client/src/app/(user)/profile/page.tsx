686c3c8d94ae946265efe76578a9ff318f1c3074
HashirMusthafa <hashirvaliyil8146@gmail.com>
2025-10-05 10:45:37 +0530

    Feat: Added renter profile page

---
+++ Client/src/app/(user)/profile/page.tsx
@@ -0,0 +1,36 @@
+import UserProfilePage from "@/components/renter/containers/Profile/ProfilePage";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Profile | Ijara Hub Premium Room & Car Rentals",
+  description:
+    "Manage your profile and account settings on Ijara Hub, your trusted platform for premium room and car rentals.",
+  keywords: [
+    "Ijara Hub",
+    "Profile",
+    "Account Settings",
+    "Room Rentals",
+    "Car Rentals",
+    "User Profile",
+    "Manage Profile",
+    "Rental Services",
+    "Premium Rentals",
+    "Ijara Hub Profile",
+    "Ijara Hub Account",
+    "Ijara Hub Settings",
+    "Ijara Hub User",
+    "Ijara Hub Rentals",
+    "Ijara Hub Room Rentals",
+  ],
+};
+
+const UserProfile = () => {
+  return (
+    <div>
+      <UserProfilePage />
+    </div>
+  );
+};
+
+export default UserProfile;
